b359ebdaca2112113c8220244c9b99e6dacf0b94
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-09-06 17:33:04 +0530

    Ideas Route added

---
+++ src/pages/Ideas/Ideas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Ideas.css";
+
+const Ideas = () => {
+    return(
+        <div className="ideas">
+            <div className="ideas__form">
+                <h1>Pitch us your idea!</h1>
+            </div>
+            <div className="ideas__model">
+                <h1>Three.js Model</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Ideas;
