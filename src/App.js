a8a314cf99331d468461d2545642c048def93af9
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-09-06 17:32:04 +0530

    added exact property to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import HomePage from "./pages/Home";
+import Ideas from "./pages/Ideas/Ideas";
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Switch>
-              <Route path='/'>
+              <Route path='/' exact>
                   <HomePage />
+              </Route>
+              <Route path='/ideas' exact>
+                  <Ideas />
               </Route>
           </Switch>
       </Router>
