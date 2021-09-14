fdadecc0d8472313f50c86beb869feaf1807c50a
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-09-14 17:57:06 +0530

    Ideas Page

--- src/pages/Ideas/Ideas.js
+++ src/pages/Ideas/Ideas.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import SideDrawer from "../../components/Navbar/SideDrawer/SideDrawer";
 import "./Ideas.css";
+import IdeasModel from "./IdeasModel";
 
 const Ideas = () => {
-    return(
-        <div className="ideas">
-            <div className="ideas__form">
-                <h1>Pitch us your idea!</h1>
-            </div>
-            <div className="ideas__model">
-                <h1>Three.js Model</h1>
-            </div>
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
+  };
+
+  return (
+    <>
+      <Navbar hamburgerClicked={sideDrawerToggleHandler} />
+      <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
+      <div className="ideas">
+        <div className="ideas__form">
+          <h1>Pitch us your idea!</h1>
         </div>
-    );
-}
+        <div className="ideas__model">
+          <IdeasModel />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Ideas;
