bb47d35af45ce203930e283b6e1138c8f39fb5da
Anuj-Rathore24 <anujsingh2409@gmail.com>
2021-09-21 22:20:30 +0530

    Event page done!!!

---
+++ src/pages/Events/Events_item.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Item_maker from "./Item_maker";
+
+let arr = [
+  {
+    Number: 0,
+    Title: "Event A",
+    Desc: "Hello",
+  },
+  {
+    Number: 1,
+    Title: "Event B",
+    Desc: "Hello",
+  },
+  {
+    Number: 2,
+    Title: "Event C",
+    Desc: "Hello",
+  },
+  {
+    Number: 3,
+    Title: "Event D",
+    Desc: "Hello",
+  },
+  {
+    Number: 4,
+    Title: "Event E",
+    Desc: "Hello",
+  },
+  {
+    Number: 5,
+    Title: "Event F",
+    Desc: "Hello",
+  },
+];
+
+export default function Events_item() {
+  return (
+    <div id="Main_Item_Container">
+      {arr.map((element) => {
+        return <Item_maker arg={element} />;
+      })}
+    </div>
+  );
+}
