754d3cb29a52c1dcf5b628bec3b90fe9f5b3b4ba
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-09-28 21:43:04 +0530

    Recruitment form page condition added

--- src/pages/Recruitment/Recruitment.js
+++ src/pages/Recruitment/Recruitment.js
@@ -4,20 +4,29 @@ import SideDrawer from "../../components/Navbar/SideDrawer/SideDrawer";
 import './Recruitment.css'
 
 const RecruitmentPage = (props) => {
+  let vhvalue = false;
+  const formOpen = false;
   const current = new Date();
   const currentMonth = current.getMonth() + 1;
+  console.log(currentMonth);
   const currentDate = current.getDate();
+  console.log(typeof(currentDate));
+  if ((currentMonth === 10) && (currentDate >=1 && currentDate <= 31)) {
+    vhvalue=true
+  }
+  
   return (
     <>
       
-      <div className="recruitment__page">
+      <div className="recruitment__page" style={{height: `${vhvalue ? '200vh' : '100vh'}`}}>
         <div className="recruitment__info__div">
             <p className="recruitment__info__header">WELCOME TO INNOVATIONAL PARADISE!</p>
             <p className="recruitment__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat ligula nec massa dignissim pretium. Integer diam nisi, ultricies at ex quis, commodo dictum sapien. Donec volutpat auctor erat, at ullamcorper enim consectetur ac. Quisque lacus nunc, sagittis ut rhoncus sed, ultricies et nulla. Donec tincidunt est quam, quis laoreet odio porta ac. Nam eleifend viverra euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eget semper enim.</p>
         </div>
-        <div className="recruitment__form__div">
+        { (currentMonth === 10) && (currentDate >=1 && currentDate <= 31) ? <div className="recruitment__form__div">
           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKJaSLZ78E0THNSk_4WkVaFyKiVT9Ft270xLR-b1OL4qn3dw/viewform?embedded=true" width="50%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-        </div>
+        </div> : <div className="recruitment__form__soon">
+          Our Recruitment will begin soon. Stay tuned!</div>}
       </div>
     </>
   );
