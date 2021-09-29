1f9499fb751aede8da9abdcf0e1cff0473be8086
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-09-29 21:30:25 +0530

    Added recruitment content

--- src/pages/Recruitment/Recruitment.js
+++ src/pages/Recruitment/Recruitment.js
@@ -21,7 +21,7 @@ const RecruitmentPage = (props) => {
       <div className="recruitment__page" style={{height: `${vhvalue ? '200vh' : '100vh'}`}}>
         <div className="recruitment__info__div">
             <p className="recruitment__info__header">WELCOME TO INNOVATIONAL PARADISE!</p>
-            <p className="recruitment__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat ligula nec massa dignissim pretium. Integer diam nisi, ultricies at ex quis, commodo dictum sapien. Donec volutpat auctor erat, at ullamcorper enim consectetur ac. Quisque lacus nunc, sagittis ut rhoncus sed, ultricies et nulla. Donec tincidunt est quam, quis laoreet odio porta ac. Nam eleifend viverra euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eget semper enim.</p>
+            <p className="recruitment__info">Wanna join the hustlers to unlock your true potential as an innovator, engineer, coder, developer, designer, content writer, manager or literally anything you could imagine? Apply to become a part of the most exclusive pioneers in engineering and its expanded sectors with IH!</p>
         </div>
         { (currentMonth === 9) && (currentDate >=1 && currentDate <= 31) ? <div className="recruitment__form__div">
           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKJaSLZ78E0THNSk_4WkVaFyKiVT9Ft270xLR-b1OL4qn3dw/viewform?embedded=true" width="50%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
