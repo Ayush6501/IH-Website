7833b3bd27fb18b432e0322f8d8877712be399cb
Rugved Borade <89184620+Bisleri06@users.noreply.github.com>
2021-10-06 06:46:12 +0000

    events,achievements,gallery, projects shifted to imgur

--- src/store/GalleryStore.js
+++ src/store/GalleryStore.js
@@ -1,47 +1,47 @@
 const GalleryStore=[{
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F1.png?alt=media&token=c5775709-6884-4681-88a7-fc934a06440e",
+  img:"https://i.imgur.com/pO9XC7O.jpg",
   txt:"Taking steps towards implementing and changing ideas by involving the correct game-changers."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F2.png?alt=media&token=0b8b656e-e00b-4f00-9f5a-43f3c8389f87",
+  img:"https://i.imgur.com/ziLehi0.jpg",
   txt:"A shining moment with the team during the World Robotics Championship."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F3.png?alt=media&token=21f2560e-3fc0-43f4-980c-2498e1448489",
+  img:"https://i.imgur.com/GXBFCOB.jpg",
   txt:"Bringing ideas to life together in large-scale events like hackathons."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F4.png?alt=media&token=f8fe4ef1-4b7a-43d0-99e6-4abab36c9e01",
+  img:"https://i.imgur.com/XA7J5r9.jpg",
   txt:"Building newer ideas and challenging existing norms on every step."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F5.png?alt=media&token=aae5023b-5e29-447b-acf3-6a176b0c9e28",
+  img:"https://i.imgur.com/Q6ZRxGq.jpg",
   txt:"Newer projects with a zest of innovation, critical thinking and hard work."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F6.png?alt=media&token=7573fadf-41e3-4211-8e54-e98081c0370e",
+  img:"https://i.imgur.com/WKMIhKn.jpg",
   txt:"Today we stand tall and proud with the team that got us here. Our team is our identity and we stand proudly alongside them on each step."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F7.png?alt=media&token=98c40154-cd27-4a17-b112-6b95ffd44828",
+  img:"https://i.imgur.com/UWSa4cc.jpg",
   txt:"Team Innovators etching their marks of success at Technoxian."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F8.png?alt=media&token=2083dd9b-7818-46fe-bf49-b0d1b5cffc9f",
+  img:"https://i.imgur.com/J4HV3wA.jpg",
   txt:"We are not just building ideas but memories. Our team is not just a group of workers but our family."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F9.png?alt=media&token=a5b29762-fb9d-4687-9d7e-4ed590a05513",
+  img:"https://i.imgur.com/TcxL8FR.jpg",
   txt:"The grind never ends, Team Innovators back at it with bigger and better ideas."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F10.png?alt=media&token=edaad17b-536f-4465-8ba7-44223e22a6e8",
+  img:"https://i.imgur.com/Yyney65.jpg",
   txt:"Bringing together the best events with the best team. Team Innovators back at it."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F11.png?alt=media&token=dd668673-c908-4e50-87af-de44bd64fcbf",
+  img:"https://i.imgur.com/edQIina.jpg",
   txt:"We just don’t say welcome to possible, we believe in it. There are no limits when it comes to innovation."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F12.png?alt=media&token=efe41c8a-5edd-462d-b0b8-b38f604cf862",
+  img:"https://i.imgur.com/hpIOGem.jpg",
   txt:"Our annual event Inception Talks being graced with the best speakers."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F13.png?alt=media&token=fb36d912-60f8-489c-99fc-757c79dddf94",
+  img:"https://i.imgur.com/5tJVStj.jpg",
   txt:"Smiling through the steps of product innovation."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F14.png?alt=media&token=dcaa20ff-c81b-4d83-90ed-eeacf73f44c3",
+  img:"https://i.imgur.com/9IMRzIq.jpg",
   txt:"Round tables, coffee, hardwork and dedication are our four essentials."},
   {
-  img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/gallery%2F15.png?alt=media&token=d5c76956-de8a-4a2d-8552-f27696e5efff",
+  img:"https://i.imgur.com/3uCxCeT.jpg",
   txt:"Not even a global pandemic could stop our ever-innovating spirit!"}
 ]
 
@@ -49,59 +49,59 @@ const GalleryStore=[{
 
 const AchievementStore=[
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F1.jpg?alt=media&token=5de0d364-dac3-462c-91f1-8820fc9808c7",
+    img:"https://i.imgur.com/DYLtkvl.jpg",
     title:"1st Place in AICTE's Smart Hackathon 2019",
     txt:"The project surrounding air quality determination secured the 1st prize in AICTE's Smart Hackathon, 2019. The system was recognized and also implemented in MIT-WPU."
   },
   
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F9.jpg?alt=media&token=1fb123e8-33d3-4be4-96b0-e5035c5d44ea",
+    img:"https://i.imgur.com/BJhRqOx.jpg",
     title:"Outstanding Performance in Ilinks Hackathons",
     txt:"Our teams performed significantly in 2 of the hackathons conducted by links digital. One of the competitions being a 24-hour hackathon, our team coded a smart parking management system under the time constraint."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F3.jpg?alt=media&token=2fe91b9c-f7a6-46a1-8018-bfd53accbdf9",
+    img:"https://i.imgur.com/rSLaZqd.jpg",
     title:"1st place in VIT Chennai's Roboprix 2021 Ideathon",
     txt:"Project CoBAND-19 which aimed to tackle the issue of managing students after college reopening through health tracking bands won a cash prize of Rs. 5000/- after securing the first position in the national competition."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F2.jpg?alt=media&token=b02f54e8-2c92-4254-b1c4-4e30de04dbd5",
+    img:"https://i.imgur.com/kxx1ytb.jpg",
     title:"1st Place Texephyr Ideathon - MIT-WPU",
     txt:"Project Younion, done by the First-Year students of our club, secured 1st place in the MIT-WPU Texephyr Ideathon. They devised a platform for professional connections catering to students and budding industrialists. The team received a cash prize of ₹ 5000/- alongside recognition by the ECE department and potential incubators of MIT-WPU."
   },
   
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F6.jpg?alt=media&token=c7a8714c-9223-4107-a4e1-c937a02350f1",
+    img:"https://i.imgur.com/Sqla3JD.jpg",
     title:"4th Place in AICTE's Samadhan Hackathon",
     txt:"Project NGO Network secured the 4th place in the Samadhan hackathon amongst 2500+ teams, conducted by AICTE in the Covid-19 lockdown. This project later made it to the last round of the Manipal Hackathon."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F7.jpg?alt=media&token=7d5c062e-ac9b-48ff-9a62-908aacec3c2c",
+    img:"https://i.imgur.com/8NKYgPv.jpg",
     title:"Special Mention in Manipal Hackathon 2020",
     txt:"Project FiN-Friend in Need got a special mention in the Manipal Hackathon among 150+ teams alongside a cash prize of ₹10,000/-. This project was appreciated & acknowledged by incubators and college authorities."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F5.jpg?alt=media&token=1adf2345-67c1-4aef-8e0b-b3e4aa9604ca",
+    img:"https://i.imgur.com/ZERShbS.jpg",
     title:"3rd Place in Envisage' 21 organized by VIT Chennai's HackClub",
     txt:"The project GYAANecology which aimed to resolve the issues faced by underprivileged women through a one fits all solution application won 3rd place, which was the best design category under the stringent 3-hour constraint addition format of the competition. They bagged cash prizes, goodies, and merit certificates for their application and the project's unique build."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F2.jpg?alt=media&token=b02f54e8-2c92-4254-b1c4-4e30de04dbd5",
+    img:"https://i.imgur.com/kxx1ytb.jpg",
     title:"2nd Place in Texephyr Ideathon MIT-WPU",
     txt:"Project Automatic Sanitization device got the first runner-up position in the MIT-WPU Texephyr Ideathon. The team received a cash price of ₹ 2500/- alongside recognition for its compelling business plan."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F4.jpg?alt=media&token=bcd095c3-9b55-4d9a-b5a2-64ce1cb92ea5",
+    img:"https://imgur.com/hZrf99W.jpg",
     title:"3rd Place in Times Technoxian ’19 WRC Innovation Contest",
     txt:"Project I-Traffic won a cash prize of ₹15,000/- in the 2019 edition of Times Technoxian. This project was also recognized & acknowledged by the college authorities and Vice-Chancellor of MIT-WPU."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F5.jpg?alt=media&token=1adf2345-67c1-4aef-8e0b-b3e4aa9604ca",
+    img:"https://i.imgur.com/ZERShbS.jpg",
     title:"Special mention in Envisage’ 21 organized by VIT Chennai’s HackClub",
     txt:"One of our teams received a special mention for their creativity in the 2021 edition of VIT-C’s Envisage hackathon. They devised a WorkFlow Alleviation System in which labour/migration classes are prevented from exploitation, given fair wages and their workflow is optimized."
   },
   {
-    img:"https://firebasestorage.googleapis.com/v0/b/ihwebsite.appspot.com/o/achievements%2F8.jpg?alt=media&token=6743342a-6c63-4e76-bab4-41a2ae57258a",
+    img:"https://i.imgur.com/MJ7ItU8.jpg",
     title:"5th Place in SRM IEEE Hacktrix",
     txt:"Project Anti-Theft Device for two-wheelers got the 5th place in the SRM IEEE hackathon for electronics. The project was also recognized in several other hackathons, jury, and audience sections as the NEED OF THE HOUR."
   }
