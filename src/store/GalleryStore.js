aab6d670328f755f62b720427a6d04d292cff03a
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-04-12 01:29:53 +0530

    added all achievements

--- src/store/GalleryStore.js
+++ src/store/GalleryStore.js
@@ -1,111 +1,198 @@
-const GalleryStore=[{
-  img:"https://i.imgur.com/pO9XC7O.jpg",
-  txt:"Taking steps towards implementing and changing ideas by involving the correct game-changers."},
+const GalleryStore = [
   {
-  img:"https://i.imgur.com/ziLehi0.jpg",
-  txt:"A shining moment with the team during the World Robotics Championship."},
-  {
-  img:"https://i.imgur.com/GXBFCOB.jpg",
-  txt:"Bringing ideas to life together in large-scale events like hackathons."},
-  {
-  img:"https://i.imgur.com/XA7J5r9.jpg",
-  txt:"Building newer ideas and challenging existing norms on every step."},
-  {
-  img:"https://i.imgur.com/Q6ZRxGq.jpg",
-  txt:"Newer projects with a zest of innovation, critical thinking and hard work."},
-  {
-  img:"https://i.imgur.com/WKMIhKn.jpg",
-  txt:"Today we stand tall and proud with the team that got us here. Our team is our identity and we stand proudly alongside them on each step."},
-  {
-  img:"https://i.imgur.com/UWSa4cc.jpg",
-  txt:"Team Innovators etching their marks of success at Technoxian."},
-  {
-  img:"https://i.imgur.com/J4HV3wA.jpg",
-  txt:"We are not just building ideas but memories. Our team is not just a group of workers but our family."},
-  {
-  img:"https://i.imgur.com/TcxL8FR.jpg",
-  txt:"The grind never ends, Team Innovators back at it with bigger and better ideas."},
-  {
-  img:"https://i.imgur.com/Yyney65.jpg",
-  txt:"Bringing together the best events with the best team. Team Innovators back at it."},
-  {
-  img:"https://i.imgur.com/edQIina.jpg",
-  txt:"We just don’t say welcome to possible, we believe in it. There are no limits when it comes to innovation."},
-  {
-  img:"https://i.imgur.com/hpIOGem.jpg",
-  txt:"Our annual event Inception Talks being graced with the best speakers."},
-  {
-  img:"https://i.imgur.com/5tJVStj.jpg",
-  txt:"Smiling through the steps of product innovation."},
-  {
-  img:"https://i.imgur.com/9IMRzIq.jpg",
-  txt:"Round tables, coffee, hardwork and dedication are our four essentials."},
-  {
-  img:"https://i.imgur.com/3uCxCeT.jpg",
-  txt:"Not even a global pandemic could stop our ever-innovating spirit!"}
-]
-
-
-
-const AchievementStore=[
-  {
-    img:"https://i.imgur.com/DYLtkvl.jpg",
-    title:"1st Place in AICTE's Smart Hackathon 2019",
-    txt:"The project surrounding air quality determination secured the 1st prize in AICTE's Smart Hackathon, 2019. The system was recognized and also implemented in MIT-WPU."
-  },
-  
-  {
-    img:"https://i.imgur.com/BJhRqOx.jpg",
-    title:"Outstanding Performance in Ilinks Hackathons",
-    txt:"Our teams performed significantly in 2 of the hackathons conducted by links digital. One of the competitions being a 24-hour hackathon, our team coded a smart parking management system under the time constraint."
+    img: "https://i.imgur.com/pO9XC7O.jpg",
+    txt: "Taking steps towards implementing and changing ideas by involving the correct game-changers.",
   },
   {
-    img:"https://i.imgur.com/rSLaZqd.jpg",
-    title:"1st place in VIT Chennai's Roboprix 2021 Ideathon",
-    txt:"Project CoBAND-19 which aimed to tackle the issue of managing students after college reopening through health tracking bands won a cash prize of Rs. 5000/- after securing the first position in the national competition."
+    img: "https://i.imgur.com/ziLehi0.jpg",
+    txt: "A shining moment with the team during the World Robotics Championship.",
   },
   {
-    img:"https://i.imgur.com/kxx1ytb.jpg",
-    title:"1st Place Texephyr Ideathon - MIT-WPU",
-    txt:"Project Younion, done by the First-Year students of our club, secured 1st place in the MIT-WPU Texephyr Ideathon. They devised a platform for professional connections catering to students and budding industrialists. The team received a cash prize of ₹ 5000/- alongside recognition by the ECE department and potential incubators of MIT-WPU."
-  },
-  
-  {
-    img:"https://i.imgur.com/Sqla3JD.jpg",
-    title:"4th Place in AICTE's Samadhan Hackathon",
-    txt:"Project NGO Network secured the 4th place in the Samadhan hackathon amongst 2500+ teams, conducted by AICTE in the Covid-19 lockdown. This project later made it to the last round of the Manipal Hackathon."
+    img: "https://i.imgur.com/GXBFCOB.jpg",
+    txt: "Bringing ideas to life together in large-scale events like hackathons.",
   },
   {
-    img:"https://i.imgur.com/8NKYgPv.jpg",
-    title:"Special Mention in Manipal Hackathon 2020",
-    txt:"Project FiN-Friend in Need got a special mention in the Manipal Hackathon among 150+ teams alongside a cash prize of ₹10,000/-. This project was appreciated & acknowledged by incubators and college authorities."
+    img: "https://i.imgur.com/XA7J5r9.jpg",
+    txt: "Building newer ideas and challenging existing norms on every step.",
   },
   {
-    img:"https://i.imgur.com/ZERShbS.jpg",
-    title:"3rd Place in Envisage' 21 organized by VIT Chennai's HackClub",
-    txt:"The project GYAANecology which aimed to resolve the issues faced by underprivileged women through a one fits all solution application won 3rd place, which was the best design category under the stringent 3-hour constraint addition format of the competition. They bagged cash prizes, goodies, and merit certificates for their application and the project's unique build."
+    img: "https://i.imgur.com/Q6ZRxGq.jpg",
+    txt: "Newer projects with a zest of innovation, critical thinking and hard work.",
   },
   {
-    img:"https://i.imgur.com/kxx1ytb.jpg",
-    title:"2nd Place in Texephyr Ideathon MIT-WPU",
-    txt:"Project Automatic Sanitization device got the first runner-up position in the MIT-WPU Texephyr Ideathon. The team received a cash price of ₹ 2500/- alongside recognition for its compelling business plan."
+    img: "https://i.imgur.com/WKMIhKn.jpg",
+    txt: "Today we stand tall and proud with the team that got us here. Our team is our identity and we stand proudly alongside them on each step.",
   },
   {
-    img:"https://imgur.com/hZrf99W.jpg",
-    title:"3rd Place in Times Technoxian ’19 WRC Innovation Contest",
-    txt:"Project I-Traffic won a cash prize of ₹15,000/- in the 2019 edition of Times Technoxian. This project was also recognized & acknowledged by the college authorities and Vice-Chancellor of MIT-WPU."
+    img: "https://i.imgur.com/UWSa4cc.jpg",
+    txt: "Team Innovators etching their marks of success at Technoxian.",
   },
   {
-    img:"https://i.imgur.com/ZERShbS.jpg",
-    title:"Special mention in Envisage’ 21 organized by VIT Chennai’s HackClub",
-    txt:"One of our teams received a special mention for their creativity in the 2021 edition of VIT-C’s Envisage hackathon. They devised a WorkFlow Alleviation System in which labour/migration classes are prevented from exploitation, given fair wages and their workflow is optimized."
+    img: "https://i.imgur.com/J4HV3wA.jpg",
+    txt: "We are not just building ideas but memories. Our team is not just a group of workers but our family.",
   },
   {
-    img:"https://i.imgur.com/MJ7ItU8.jpg",
-    title:"5th Place in SRM IEEE Hacktrix",
-    txt:"Project Anti-Theft Device for two-wheelers got the 5th place in the SRM IEEE hackathon for electronics. The project was also recognized in several other hackathons, jury, and audience sections as the NEED OF THE HOUR."
-  }
+    img: "https://i.imgur.com/TcxL8FR.jpg",
+    txt: "The grind never ends, Team Innovators back at it with bigger and better ideas.",
+  },
+  {
+    img: "https://i.imgur.com/Yyney65.jpg",
+    txt: "Bringing together the best events with the best team. Team Innovators back at it.",
+  },
+  {
+    img: "https://i.imgur.com/edQIina.jpg",
+    txt: "We just don’t say welcome to possible, we believe in it. There are no limits when it comes to innovation.",
+  },
+  {
+    img: "https://i.imgur.com/hpIOGem.jpg",
+    txt: "Our annual event Inception Talks being graced with the best speakers.",
+  },
+  {
+    img: "https://i.imgur.com/5tJVStj.jpg",
+    txt: "Smiling through the steps of product innovation.",
+  },
+  {
+    img: "https://i.imgur.com/9IMRzIq.jpg",
+    txt: "Round tables, coffee, hardwork and dedication are our four essentials.",
+  },
+  {
+    img: "https://i.imgur.com/3uCxCeT.jpg",
+    txt: "Not even a global pandemic could stop our ever-innovating spirit!",
+  },
 ];
 
+const AchievementStore = [
+  {
+    img: "https://i.imgur.com/kxx1ytb.jpg",
+    title: "1st Place Texephyr Ideathon - MIT-WPU",
+    txt: "Project Younion, done by the First-Year students of our club, secured 1st place in the MIT-WPU Texephyr Ideathon. They devised a platform for professional connections catering to students and budding industrialists. The team received a cash prize of ₹ 5000/- alongside recognition by the ECE department and potential incubators of MIT-WPU.",
+  },
+  {
+    img: "https://i.imgur.com/kxx1ytb.jpg",
+    title: "2nd Place in Texephyr Ideathon MIT-WPU",
+    txt: "Project Automatic Sanitization device got the first runner-up position in the MIT-WPU Texephyr Ideathon. The team received a cash price of ₹ 2500/- alongside recognition for its compelling business plan.",
+  },
+  {
+    img: "https://i.imgur.com/rSLaZqd.jpg",
+    title: "1st place in VIT Chennai's Roboprix 2021 Ideathon",
+    txt: "Project CoBAND-19 which aimed to tackle the issue of managing students after college reopening through health tracking bands won a cash prize of Rs. 5000/- after securing the first position in the national competition.",
+  },
+  {
+    img: "https://i.imgur.com/2Q2Znvg.jpg",
+    title: "2nd Place in MSIT's New Normal Ideathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/ZERShbS.jpg",
+    title: "3rd Place in Envisage' 21 organized by VIT Chennai's HackClub",
+    txt: "The project GYAANecology which aimed to resolve the issues faced by underprivileged women through a one fits all solution application won 3rd place, which was the best design category under the stringent 3-hour constraint addition format of the competition. They bagged cash prizes, goodies, and merit certificates for their application and the project's unique build.",
+  },
+  {
+    img: "https://imgur.com/hZrf99W.jpg",
+    title: "3rd Place in Times Technoxian ’19 WRC Innovation Contest",
+    txt: "Project I-Traffic won a cash prize of ₹15,000/- in the 2019 edition of Times Technoxian. This project was also recognized & acknowledged by the college authorities and Vice-Chancellor of MIT-WPU.",
+  },
+  {
+    img: "https://i.imgur.com/Sqla3JD.jpg",
+    title: "4th Place in AICTE's Samadhan Hackathon",
+    txt: "Project NGO Network secured the 4th place in the Samadhan hackathon amongst 2500+ teams, conducted by AICTE in the Covid-19 lockdown. This project later made it to the last round of the Manipal Hackathon.",
+  },
+  {
+    img: "https://i.imgur.com/8NKYgPv.jpg",
+    title: "Special Mention in Manipal Hackathon 2020",
+    txt: "Project FiN-Friend in Need got a special mention in the Manipal Hackathon among 150+ teams alongside a cash prize of ₹10,000/-. This project was appreciated & acknowledged by incubators and college authorities.",
+  },
+  {
+    img: "https://i.imgur.com/MJ7ItU8.jpg",
+    title: "5th Place in SRM IEEE Hacktrix",
+    txt: "Project Anti-Theft Device for two-wheelers got the 5th place in the SRM IEEE hackathon for electronics. The project was also recognized in several other hackathons, jury, and audience sections as the NEED OF THE HOUR.",
+  },
+  {
+    img: "https://i.imgur.com/BJhRqOx.jpg",
+    title: "Outstanding Performance in Ilinks Hackathons",
+    txt: "Our teams performed significantly in 2 of the hackathons conducted by links digital. One of the competitions being a 24-hour hackathon, our team coded a smart parking management system under the time constraint.",
+  },
+  {
+    img: "https://i.imgur.com/wVYJ8qY.jpg",
+    title: "Final Round in IBM Reimagine Future Hackathon - Team Anthropology",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/wVYJ8qY.jpg",
+    title: "Final Round in IBM Reimagine Future Hackathon - The pony place",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/ihQcL5I.jpg",
+    title: "Top 26 teams in Smart Homes Hacakthon Resideo 21'",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/va9LGrS.jpg",
+    title: "1st Place secured by Rushikesh Patade in Code Of Duty - GDSC 2021",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/va9LGrS.jpg",
+    title: "3rd Place secured by Viswas Haridas in Code Of Duty - GDSC 2021",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/va9LGrS.jpg",
+    title: "4th Place secured by Muskan Singh in Code Of Duty - GDSC 2021",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/ygDuCdm.jpg",
+    title: "Top 20 teams in TDRA ICT Hackathon 2021",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/DYLtkvl.jpg",
+    title: "1st Place in AICTE's Smart Hackathon 2019",
+    txt: "The project surrounding air quality determination secured the 1st prize in AICTE's Smart Hackathon, 2019. The system was recognized and also implemented in MIT-WPU.",
+  },
+  {
+    img: "https://i.imgur.com/ZERShbS.jpg",
+    title:
+      "Special mention in Envisage’ 21 organized by VIT Chennai’s HackClub",
+    txt: "One of our teams received a special mention for their creativity in the 2021 edition of VIT-C’s Envisage hackathon. They devised a WorkFlow Alleviation System in which labour/migration classes are prevented from exploitation, given fair wages and their workflow is optimized.",
+  },
+  {
+    img: "https://i.imgur.com/Ys9lbmV.jpg",
+    title: "Special mention in E-Yantra Hackathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/i8dm1LX.jpg",
+    title: "1st Place in Kotak Reign Hackathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/i8dm1LX.jpg",
+    title: "2nd Place in Kotak Reign Hackathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/i8dm1LX.jpg",
+    title: "Top 22 in Kotak Reign Hackathon",
+    txt: "Team 404Found proposed a prototype to reimagine the banking dashboard of Kotak Mahindra Bank for the next generation. The proposal was shortlisted in the top 22 submissions out of 5000 entries ranging from over 1200+ colleges in India.",
+  },
+  {
+    img: "https://i.imgur.com/hfYJfr6.jpg",
+    title: "Top 26 in Tibco IOT Hackathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/68zSPwC.jpg",
+    title: "Reached 2nd Round in Y-Hills Hackathon",
+    txt: "Project desc here",
+  },
+  {
+    img: "https://i.imgur.com/xFcsJle.jpg",
+    title: "6th Place in AR Atelier Hackathon",
+    txt: "Project desc here",
+  },
+];
 
-export {GalleryStore,AchievementStore};
+export { GalleryStore, AchievementStore };
