279b907dacfa100b147b3eb076ca84203fa1bc00
Ayush6501 <ayushmajumdar6501@gmail.com>
2022-04-12 07:52:00 +0530

    Added alumni page draft

--- src/pages/Alumini/alumini.js
+++ src/pages/Alumini/alumini.js
@@ -1,29 +1,28 @@
 import styled from 'styled-components';
 import styles from './alumini.module.css'
 import {useRef, useEffect} from 'react';
-import { useLocation} from "react-router-dom";
-import { Link } from 'react-router-dom';
-import { Box,Typography } from '@mui/material';
+import {useHistory, useLocation} from "react-router-dom";
+import Divider from '@mui/material/Divider';
+import AlumniData from "./AlumniData";
 
-function Alumini(props){
+function Alumini(props) {
     const ref = useRef();
-    const routePath = useLocation();
+    const routePath = useLocation()
+    console.log(AlumniData)
 
     useEffect(() => {
         if (routePath.pathname === "/about") {
-            ref.current.scrollIntoView({ behavior: "smooth" });
+            ref.current.scrollIntoView({behavior: "smooth"});
         }
     }, [routePath])
 
     return (<div className={styles.Main} ref={ref}>
 
-        <img src="alumni.jpg" 
-          style={{position:"relative",objectFit:"cover",top:0,left:0,height:"60vh",width:"100vw",marginBottom:"-14rem"}}
-        ></img>
-
-        <Box sx={{position:"relative"}}>
-          <Typography variant="h2" fontFamily={"Poppins"} fontWeight={"bold"} color="white" align="center" marginTop="8rem" marginBottom="12rem">OUR ALUMNI</Typography>
-        </Box>
+        <Parent>
+            <HeaderCard>
+                <h1>OUR ALUMINI</h1>
+            </HeaderCard>
+        </Parent>
 
         <div className={styles.DepartmentButtonContainer}>
             <span className={styles.DepartmentButton}>
@@ -32,68 +31,28 @@ function Alumini(props){
 
         </div>
         <div className={styles.TeamWrapper}>
-            {test_data.map((i) => <TeamMember key={i.index} imgsrc={i["Image ( min : 600 x 600 pixels )"]} name={i["Full Name"]} desig={i["Position"]} dept={i["Department(s)"]} linkedin={i["LinkedIn"]} github={i["GitHub ( mandatory for CSE )"]} />)}
+            {AlumniData.map((i) => <TeamMember
+                key={i["Sr No."]}
+                imgsrc={i["Image"]}
+                name={i["Name"]}
+                desig={i["Position"]}
+                dept={i["Department"]}
+                linkedin={i["Linkedin"]}
+            />)}
         </div>
-        </div>);
+    </div>);
 }
-
-
-const test_data=[
-    {
-        "Department(s)" : "CSE",
-        "Email Address" : "",
-        "Full Name" : "Digvijay Patil",
-        "GitHub ( mandatory for CSE )" : "https://github.com/think-digvijay",
-        "Image ( min : 600 x 600 pixels )" : "https://i.imgur.com/h5MRQTj.png",
-        "LinkedIn" : "https://www.linkedin.com/in/23patildigvijay/",
-        "Position" : "Founder",
-        "Timestamp" : "",
-        "Year Of Study" : "Core"
-      }, {
-        "Department(s)" : "Mechanical",
-        "Email Address" : "phadtare.mohit@gmail.com",
-        "Full Name" : "Mohit Phadtare",
-        "GitHub ( mandatory for CSE )" : "",
-        "Image ( min : 600 x 600 pixels )" : "https://i.imgur.com/2M9jJh2.png",
-        "LinkedIn" : "https://www.linkedin.com/in/mohit-phadtare/",
-        "Position" : "Founder",
-        "Timestamp" : "9/29/2021 22:21:54",
-        "Year Of Study" : "Core"
-      }, {
-        "Department(s)" : "Non-Tech",
-        "Email Address" : "vedpatil10@gmail.com",
-        "Full Name" : "Vedang Patil",
-        "GitHub ( mandatory for CSE )" : "",
-        "Image ( min : 600 x 600 pixels )" : "https://i.imgur.com/jKUgfk4.png",
-        "LinkedIn" : "https://www.linkedin.com/in/vedang-patil-a57b6719a",
-        "Position" : "Founder",
-        "Timestamp" : "9/28/2021 17:04:05",
-        "Year Of Study" : "Core"
-      }, {
-        "Department(s)" : "CSE, Non-Tech",
-        "Email Address" : "",
-        "Full Name" : "Aryan Yadav",
-        "GitHub ( mandatory for CSE )" : "https://github.com/AryanYadav9999",
-        "Image ( min : 600 x 600 pixels )" : "https://i.imgur.com/5UfKvdZ.png",
-        "LinkedIn" : "https://www.linkedin.com/in/aryanyadav9999/",
-        "Position" : "President",
-        "Timestamp" : "",
-        "Year Of Study" : "Core"
-      }
-]
-
-
 
 function TeamMember(props) {
     let imgsrc = props.imgsrc;
     let name = props.name;
     let desig = props.desig;
     let linkedin = props.linkedin;
-    let dept=props.dept;
-    let lnk="/alumini/"+name.replace(" ","").toLowerCase();
+    let dept=props.dept
+    const loc = useHistory();
 
     return (
-      <div className={styles.TeamCard}>
+      <div className={styles.TeamCard} onClick={()=>loc.push('/alumni/' + name)}>
         <img src={imgsrc} alt="Error" className={styles.MemberImage} />
         <div className={styles.MemberOverlay}>
           <p className={styles.MemberRank}>{desig}</p>
@@ -102,8 +61,7 @@ function TeamMember(props) {
         </div>
 
         <div className={styles.MemberName}>{name}</div>
-      </div>
-      );
+      </div>);
 }
 
 
--- src/pages/AlumniMembers.js
+++ src/pages/AlumniMembers.js
@@ -1,14 +1,106 @@
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
+import AlumniData from "./Alumini/AlumniData";
 
 const AlumniMember = () => {
     const params = useParams()
-    console.log(params)
+    let obj = AlumniData.find(o => o["Name"] === params.name);
+    console.log(obj);
 
     return (
-        <div style={{ height: "100vh", marginTop: '60px'}}>
-            <h1>Alumni Member Page of {params.name}</h1>
-        </div>
+        <MainContainer>
+            <Container>
+                <ImageDetail>
+                    <ImageContainer>
+                        <img src={obj["Image"]} alt=""/>
+                    </ImageContainer>
+                    <DetailsContainer>
+                        <h1>{obj["Name"]}</h1>
+                        <h2>{obj["Position in Club"]}</h2>
+                        <h2>{obj["Department"]} ({obj["Start Year"]} - {obj["End Year"]} )</h2>
+                        <h2>Email ID: {obj["Email ID"]}</h2>
+                        <h2>Current Status: {obj["Current Status"]}</h2>
+                        <IconContainer>
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                            <ion-icon name="logo-instagram"></ion-icon>
+                            <ion-icon name="mail-outline"></ion-icon>
+                            <ion-icon name="cloud-download-outline"></ion-icon>
+                        </IconContainer>
+                    </DetailsContainer>
+                </ImageDetail>
+                <ClubContainer>
+                    <img src="/Logo-1.ico" alt=""/>
+                </ClubContainer>
+            </Container>
+        </MainContainer>
     )
 }
 
 export default AlumniMember;
+
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  height: 70%;
+  width: 90%;
+  //background: blue;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 30px;
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  
+  img {
+    height: 100%;
+    margin-right: 30px;
+  }
+`;
+
+const DetailsContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  color: #000;
+  
+  h1 {
+    font-size: 3em;
+    margin: 0;
+  }
+  
+  h2 {
+    color: #000;
+    font-size: 1.5em;
+    margin: 0;
+  }
+`;
+
+const ClubContainer = styled.div`
+    
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+
+  ion-icon {
+    font-size: 48px;
+    margin-right: 10px;
+  }
+`;
+
+const ImageDetail = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 90%;
+`;
